a6310170529a8f58ea6bd9c7aed640eccbf0d50c
DeFiMan1729.eth <115624087+DefiMan1729@users.noreply.github.com>
2022-10-23 10:40:15 +0530

    Update InvoiceVault.sol

--- contracts/InvoiceVault.sol
+++ contracts/InvoiceVault.sol
@@ -1,45 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-interface IInvoice {
-function safeTransferFrom(address _from, address _to, uint256 _x) external;
-function getFace(uint256 _x) external view returns(uint256);
+// Interface for the Invoice that will be deposited to get loan
+interface IInvoice_NFT{
+  function getFaceValue(uint256 _tokenid) external returns (uint256);
+  function safeDeposit(address _to, uint256 _tokenid) external;
 }
 
-contract InvoiceVault is ERC20,Ownable{
-    IERC721 public immutable token;
-    IInvoice public immutable II;
+// Interface for the token that the depositor will receive
+interface ILoan_ERC20{
+  function loanMint(address _to,uint256 _faceValue) external;
+}
+
+contract InvoiceVault{
     address public manager;
     uint256 public tokenid;
-
-    constructor(address _token) ERC20("InvoiceFactor", "INV") {
-        token = IERC721(_token);
-        II = IInvoice(_token);
+    uint256 faceValue;
+    address invoice;
+    address loan;
+    constructor(address _NFT, address _ERC20){
+        invoice =_NFT;
+        loan = _ERC20;
         manager = msg.sender;
     }
-
-    function mint(address to, uint256 amount) public {
-        _mint(to, amount);
+    
+    function deposit (uint256 _tokenid) public{
+        IInvoice_NFT(invoice).safeDeposit(manager,_tokenid);
+        faceValue = IInvoice_NFT(invoice).getFaceValue(_tokenid);
+        ILoan_ERC20(loan).loanMint(msg.sender,faceValue);
     }
-    function getBalToken(address _adr) public view returns(uint256){
-        return balanceOf(_adr);
-    }
-// get face value
-    function getFV(address _adr, uint256 _x) public view returns(uint256){
-        return IInvoice(_adr).getFace(_x);
-    }
-
-    function deposit(uint256 _tokenid) public payable{
-        token.safeTransferFrom(msg.sender, manager, _tokenid);
-        mint(msg.sender,II.getFace(_tokenid));
-    }
-
 }
